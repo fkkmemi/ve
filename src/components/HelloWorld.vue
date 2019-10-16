a1b7275f865e1b7b0fd2f18edba4bd5f79130178
fkkmemi <fkkmemi@gmail.com>
2019-10-16 23:03:53 +0900

    3 에디터 만들어보기

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,142 +1,72 @@
 <template>
-  <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
+  <v-container fluid>
+    <v-card>
+      <v-toolbar dense flat color="primary" dark>
+        <v-toolbar-title>문서편집기</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="editMode = !editMode">
+          <v-icon>{{editMode ? 'mdi-eye' : 'mdi-pencil'}}</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-text>
+        <editor v-if="editMode" v-model="text"></editor>
+        <viewer v-else :value="text"></viewer>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="read">read</v-btn>
+        <v-btn @click="write">write</v-btn>
+      </v-card-actions>
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify 2
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+    </v-card>
   </v-container>
 </template>
-
 <script>
+import 'tui-editor/dist/tui-editor.css'
+import 'tui-editor/dist/tui-editor-contents.css'
+import 'codemirror/lib/codemirror.css'
+import { Editor, Viewer } from '@toast-ui/vue-editor'
+const { dialog } = require('electron').remote
+const fs = require('fs')
+
 export default {
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader'
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify'
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+  components: {
+    'editor': Editor,
+    'viewer': Viewer
+  },
+  data () {
+    return {
+      editMode: true,
+      text: ''
+    }
+  },
+  methods: {
+    read () {
+      const options = {
+        filters: [
+          {
+            name: 'text and markdown',
+            extensions: ['txt', 'md']
+          }
+        ]
       }
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com'
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com'
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify'
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs'
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify'
+      const r = dialog.showOpenDialogSync(options)
+      if (!r) return
+      this.text = fs.readFileSync(r[0]).toString()
+    },
+    write () {
+      const options = {
+        filters: [
+          {
+            name: 'text and markdown',
+            extensions: ['txt', 'md']
+          }
+        ]
       }
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer'
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined'
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-      }
-    ]
-  })
+      const r = dialog.showSaveDialogSync(options)
+      if (!r) return
+      fs.writeFileSync(r, this.text)
+    }
+  }
 }
 </script>

fa8732c246274c07b94f124083e125d9f24f973f
fkkmemi <fkkmemi@gmail.com>
2019-11-09 01:47:56 +0900

    파일 검색 태그 만들어보기

--- src/views/File2DB.vue
+++ src/views/File2DB.vue
@@ -16,14 +16,38 @@
         :items="items"
       >
         <template v-slot:item.name="{ item }">
-          <v-btn @click="execute(item)">{{item.name}}</v-btn>
+          <v-btn @click="execute(item)">{{item.name.substr(0, 20)}}</v-btn>
         </template>
         <template v-slot:item.time="{ item }">
           {{ new Date(item.time).toLocaleString() }}
 
         </template>
+        <template v-slot:item.size="{ item }">
+          {{ byte(item.size) }}
+        </template>
+        <template v-slot:item.tags="{ item }">
+          <v-chip v-for="tag in item.tags" :key="tag" close @click:close="delTag(item, tag)">{{tag}}</v-chip>
+          <v-btn icon @click="openDialog(item)"><v-icon>mdi-plus</v-icon></v-btn>
+        </template>
       </v-data-table>
     </v-card>
+    <v-dialog v-model="dialog" width="300">
+      <v-card v-if="selectedItem">
+        <v-card-title>tag modify</v-card-title>
+        <v-card-text>
+          <v-combobox
+            v-model="selectedItem.tags"
+            :items="tags"
+            multiple
+            chips
+            @change="changeTag"
+          >
+
+          </v-combobox>
+        </v-card-text>
+      </v-card>
+
+    </v-dialog>
   </v-container>
 </template>
 <script>
@@ -31,6 +55,7 @@ const { shell } = require('electron')
 const { dialog } = require('electron').remote
 const fs = require('fs')
 const path = require('path')
+const byte = require('bytes')
 const Datastore = require('nedb-promises')
 const DBFile = Datastore.create('dbFiles.db')
 
@@ -38,15 +63,19 @@ export default {
   data () {
     return {
       dir: '',
+      byte,
       headers: [
-        { value: '_id', text: '_id' },
+        // { value: '_id', text: '_id' },
         { value: 'name', text: 'name' },
         // { value: 'path', text: 'path' },
         { value: 'time', text: 'time' },
         { value: 'size', text: 'size' },
         { value: 'tags', text: 'tags' }
       ],
-      items: []
+      items: [],
+      dialog: false,
+      selectedItem: null,
+      tags: []
     }
   },
   mounted () {
@@ -55,6 +84,7 @@ export default {
   methods: {
     async fetch () {
       this.items = await DBFile.find()
+      this.addTag()
     },
     async read () {
       const rs = dialog.showOpenDialogSync({
@@ -82,6 +112,26 @@ export default {
     },
     execute (item) {
       shell.openItem(item.path)
+    },
+    openDialog (item) {
+      this.selectedItem = item
+      this.dialog = true
+    },
+    async changeTag () {
+      await DBFile.update({ _id: this.selectedItem._id }, { $set: { tags: this.selectedItem.tags } })
+      this.addTag()
+    },
+    addTag () {
+      this.items.forEach(item => {
+        const tags = item.tags
+        tags.forEach(tag => {
+          if (!this.tags.includes(tag)) this.tags.push(tag)
+        })
+      })
+    },
+    async delTag (item, tag) {
+      await DBFile.update({ _id: item._id }, { $pull: { tags: tag } })
+      await this.fetch()
     }
   }
 }
